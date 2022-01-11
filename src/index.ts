ef89b8d817da29825304c16cde896ad3028facbc
Jeffrey Rose <jtr219@outlook.com>
2022-01-11 09:51:42 -0500

    set up board and basic ChessPiece class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-export const tst = "";
+export type PlayerColor = "white" | "black";
+
+export const boardPlaces = [
+  [1, 2, 3, 4, 5, 6, 7, 8], // a
+  [1, 2, 3, 4, 5, 6, 7, 8], // b
+  [1, 2, 3, 4, 5, 6, 7, 8], // c
+  [1, 2, 3, 4, 5, 6, 7, 8], // d
+  [1, 2, 3, 4, 5, 6, 7, 8], // e
+  [1, 2, 3, 4, 5, 6, 7, 8], // f
+  [1, 2, 3, 4, 5, 6, 7, 8], // g
+  [1, 2, 3, 4, 5, 6, 7, 8], // h
+];
+
+class ChessPiece {
+  position: number;
+  alive: boolean;
+  color: PlayerColor;
+
+  constructor(position: number, color: PlayerColor) {
+    this.position = position;
+    this.color = color;
+    this.alive = true; // defaults to true
+  }
+}
