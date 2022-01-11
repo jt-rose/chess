0de06f6c0625bf726e94fb53c9c9b9a46febf49d
Jeffrey Rose <jtr219@outlook.com>
2022-01-11 10:57:27 -0500

    set up chess pieces

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 export type PlayerColor = "white" | "black";
 
-export let boardSpaces: number[] = [];
-
-for (let i = 1; i <= 64; i++) {
-  boardSpaces.push(i);
-}
-
+// set up a generic chess piece class
+// the move function will be overwritten with the unique move instructions
+// for each type of chess piece
 class ChessPiece {
   position: number;
   alive: boolean;
   color: PlayerColor;
+  move(): number[] {
+    return [];
+  }
 
   constructor(position: number, color: PlayerColor) {
     this.position = position;
@@ -18,15 +18,47 @@ class ChessPiece {
   }
 }
 
-// king
-// queen
-// bishop
-// knight
-// rook
-// pawn
-
-class KingPiece extends ChessPiece {
+class King extends ChessPiece {
   constructor(position: number, color: PlayerColor) {
     super(position, color);
   }
 }
+
+class Queen extends ChessPiece {
+  constructor(position: number, color: PlayerColor) {
+    super(position, color);
+  }
+}
+class Bishop extends ChessPiece {
+  constructor(position: number, color: PlayerColor) {
+    super(position, color);
+  }
+}
+class Knight extends ChessPiece {
+  constructor(position: number, color: PlayerColor) {
+    super(position, color);
+  }
+}
+
+class Rook extends ChessPiece {
+  constructor(position: number, color: PlayerColor) {
+    super(position, color);
+  }
+}
+
+class Pawn extends ChessPiece {
+  constructor(position: number, color: PlayerColor) {
+    super(position, color);
+  }
+}
+
+type ChessPieces = King | Queen | Bishop | Knight | Rook | Pawn;
+/*
+export let boardSpaces: ChessPieces | null[] = [];
+
+for (let i = 1; i <= 64; i++) {
+  boardSpaces.push(null);
+}
+
+const setBoard = () => {};
+*/
