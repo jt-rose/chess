7ff9d1b42b111001bd87e9613c3b575d5bb0cb6c
Jeffrey Rose <jtr219@outlook.com>
2022-01-11 15:11:01 -0500

    store chess pieces in separate file

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,13 @@
-type PlayerColor = "white" | "black";
-
-// set up a generic chess piece class
-// the movementOptions and move functions will be overwritten with the unique move instructions
-// for each type of chess piece
-class ChessPiece {
-  alive: boolean;
-  color: PlayerColor;
-  movementOptions(): number[] {
-    return [];
-  }
-  move(position: number): boolean {
-    // const availableSpots = this.movementOptions()
-    // if (availableSpots.includes(position)) {
-    // }
-    // return [];
-    console.log(position);
-    return false;
-  }
-
-  constructor(color: PlayerColor) {
-    this.color = color;
-    this.alive = true; // defaults to true
-  }
-}
-
-class King extends ChessPiece {
-  constructor(color: PlayerColor) {
-    super(color);
-  }
-}
-
-class Queen extends ChessPiece {
-  constructor(color: PlayerColor) {
-    super(color);
-  }
-}
-class Bishop extends ChessPiece {
-  constructor(color: PlayerColor) {
-    super(color);
-  }
-}
-class Knight extends ChessPiece {
-  constructor(color: PlayerColor) {
-    super(color);
-  }
-}
-
-class Rook extends ChessPiece {
-  constructor(color: PlayerColor) {
-    super(color);
-  }
-}
-
-class Pawn extends ChessPiece {
-  constructor(color: PlayerColor) {
-    super(color);
-  }
-}
-
-// set up general type for pieces
-type ChessPieces = King | Queen | Bishop | Knight | Rook | Pawn;
+import {
+  PlayerColor,
+  ChessPieces,
+  Rook,
+  Knight,
+  Bishop,
+  Queen,
+  King,
+  Pawn,
+} from "./pieces";
 
 // build out starting board of 64 spots with no pieces set
 let boardSpaces: (ChessPieces | null)[] = [];
---
+++ src/pieces.ts
@@ -0,0 +1,62 @@
+export type PlayerColor = "white" | "black";
+
+// set up a generic chess piece class
+// the movementOptions and move functions will be overwritten with the unique move instructions
+// for each type of chess piece
+export class ChessPiece {
+  alive: boolean;
+  color: PlayerColor;
+  movementOptions(): number[] {
+    return [];
+  }
+  move(position: number): boolean {
+    // const availableSpots = this.movementOptions()
+    // if (availableSpots.includes(position)) {
+    // }
+    // return [];
+    console.log(position);
+    return false;
+  }
+
+  constructor(color: PlayerColor) {
+    this.color = color;
+    this.alive = true; // defaults to true
+  }
+}
+
+export class King extends ChessPiece {
+  constructor(color: PlayerColor) {
+    super(color);
+  }
+}
+
+export class Queen extends ChessPiece {
+  constructor(color: PlayerColor) {
+    super(color);
+  }
+}
+export class Bishop extends ChessPiece {
+  constructor(color: PlayerColor) {
+    super(color);
+  }
+}
+export class Knight extends ChessPiece {
+  constructor(color: PlayerColor) {
+    super(color);
+  }
+}
+
+export class Rook extends ChessPiece {
+  constructor(color: PlayerColor) {
+    super(color);
+  }
+}
+
+export class Pawn extends ChessPiece {
+  constructor(color: PlayerColor) {
+    super(color);
+  }
+}
+
+// set up general type for pieces
+export type ChessPieces = King | Queen | Bishop | Knight | Rook | Pawn;
