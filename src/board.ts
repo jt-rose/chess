f14fe2a6115f2ba587007cb13a374770d7b334f8
Jeffrey Rose <jtr219@outlook.com>
2022-01-18 11:38:29 -0500

    refactor to organize board logic into new file

---
+++ src/board.ts
@@ -0,0 +1,47 @@
+import { ChessPieces, Pawn, King, Queen, Knight, Bishop, Rook } from "./pieces";
+
+// declare a board with 64 positions that are occupied by a piece or empty
+export type Board = (ChessPieces | null)[];
+
+// return a board with pieces in their starting positions
+export const createNewBoard = (): Board => {
+  let board: Board = [];
+  for (let i = 0; i < 64; i++) {
+    board.push(null);
+  }
+  // set up blacks
+  board[0] = new Rook("black");
+  board[1] = new Knight("black");
+  board[2] = new Bishop("black");
+  board[3] = new Queen("black");
+  board[4] = new King("black");
+  board[5] = new Bishop("black");
+  board[6] = new Knight("black");
+  board[7] = new Rook("black");
+
+  // set up black pawns
+  for (let i = 8; i < 16; i++) {
+    board[i] = new Pawn("black");
+  }
+
+  // set up empty spaces - useful for resetting the board for a new game
+  for (let i = 16; i < 48; i++) {
+    board[i] = null;
+  }
+  // set up white pawns
+  for (let i = 48; i < 56; i++) {
+    board[i] = new Pawn("white");
+  }
+
+  // set up whites
+  board[56] = new Rook("white");
+  board[57] = new Knight("white");
+  board[58] = new Bishop("white");
+  board[59] = new Queen("white");
+  board[60] = new King("white");
+  board[61] = new Bishop("white");
+  board[62] = new Knight("white");
+  board[63] = new Rook("white");
+
+  return board;
+};
