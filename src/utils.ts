86bd798896f2eeda1b305e31786b772aa842e2fd
Jeffrey Rose <jtr219@outlook.com>
2022-01-18 11:31:53 -0500

    add mapPossibleMoves function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import { Board, ChessPieceWithIndex } from ".";
+import { Board, ChessPieceWithIndex, updateBoard } from ".";
+import { MovementOptions } from "./path";
 import { PlayerColor } from "./pieces";
 
 export const getPiecesOfSameColor = (
@@ -15,4 +16,78 @@ export const getPiecesOfSameColor = (
   }
 
   return pieces;
+};
+
+// convert undefined movement options into empty number array
+const convertNumberArray = (nums: number[] | undefined): number[] =>
+  nums ? nums : [];
+
+const convertMovementOptionsToArray = (mo: MovementOptions): number[] => {
+  let { top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft } =
+    mo;
+
+  // convert undefined values
+  top = convertNumberArray(top);
+  topRight = convertNumberArray(topRight);
+  right = convertNumberArray(right);
+  bottomRight = convertNumberArray(bottomRight);
+  bottom = convertNumberArray(bottom);
+  bottomLeft = convertNumberArray(bottomLeft);
+  left = convertNumberArray(left);
+  topLeft = convertNumberArray(topLeft);
+  return [
+    ...top,
+    ...topRight,
+    ...right,
+    ...bottomRight,
+    ...bottom,
+    ...bottomLeft,
+    ...left,
+    ...topLeft,
+  ];
+};
+
+// get the current offensive range for one side
+export const getOffensiveRange = (
+  offensiveColor: PlayerColor,
+  board: Board
+) => {
+  // pick out pieces from board that are on the same offensive side
+  let offensivePieces = getPiecesOfSameColor(offensiveColor, board);
+
+  // get movement options for all offensive pieces
+  const offensiveMovementOptions = offensivePieces
+    .map((p) =>
+      p.getMovementOptions({ position: p.index, color: p.color, board })
+    )
+    .flatMap((mo) => convertMovementOptionsToArray(mo));
+
+  return offensiveMovementOptions;
+};
+
+// map out possible board updates after all possible moves
+export const mapPossibleMoves = (offensiveColor: PlayerColor, board: Board) => {
+  // get pieces of same color
+  const pieces = getPiecesOfSameColor(offensiveColor, board);
+
+  // map out possible movement options by piece
+  const movementOptionsByPiece = pieces.map((p) => ({
+    ...p,
+    movementOptions: convertMovementOptionsToArray(
+      p.getMovementOptions({ position: p.index, color: p.color, board })
+    ),
+  }));
+
+  // for each possible movement position of each piece, map out the board updates that would ensue
+  const possibleBoardUpdates = movementOptionsByPiece
+    .map((piece) => {
+      return piece.movementOptions.map((mo) =>
+        updateBoard(piece.index, mo, board)
+      );
+    })
+    .flat()
+    .flat();
+
+  // return possible board outcomes
+  return possibleBoardUpdates;
 };
